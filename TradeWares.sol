dd0d4345edbce09e5ccc55a5b2faf1b4cb7eb1bd
Charles E. Jason <107406861+Creatorjason@users.noreply.github.com>
2022-08-11 21:00:02 +0100

    Update TradeWares.sol

--- TradeWares.sol
+++ TradeWares.sol
@@ -1,40 +1,53 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract GroupAdmin{
-    // 0x919b81cdeba0376a3949fc362aab3fde700a2118c084972392f9455a2ec29c89
-    // 0x2db9fd3d099848027c2383d0a083396f6c41510d7acfd92adc99b6cffcf31e96
-    bytes32 private constant SUPERUSER = keccak256(abi.encodePacked("SUPERUSER"));
-    bytes32 private constant USER = keccak256(abi.encodePacked("USER"));
+interface IERC721{
+    function transferFrom(
+        address _from,
+        address _to,
+        uint _nftId
+    )
+    external ;
+}
 
+contract DutchAuction{
+    uint private constant DURATION = 1 days;
+    IERC721 public immutable nft;
+    uint public immutable nftId;
+    address payable public immutable seller;
+    uint public immutable startingPrice;
+    uint public immutable startAt;
+    uint public immutable expiresAt;
+    uint public immutable discountRate;
 
-    mapping(bytes32 => mapping(address => bool)) internal user;
+    constructor(
+        uint _startingPrice,
+        uint _discountRate,
+        address _nft,
+        uint _nftId) {
+        startingPrice = _startingPrice;
+        discountRate = _discountRate;
+        startAt = block.timestamp;
+        expiresAt = block.timestamp + DURATION;
+        nft = IERC721(_nft);
+        nftId = _nftId;
+        seller = payable(msg.sender);
+        require(_startingPrice >= _discountRate * DURATION, "Starting price is too low");}
 
-    event UserAdded(bytes32  indexed _role, address indexed _addr, string indexed _reason );
-    event UserRemoved(bytes32 indexed _role, address indexed _addr, string indexed _reason);
-
-    constructor(){
-        _grantRole(SUPERUSER, msg.sender);
+    function getPrice() public view returns(uint price){
+        uint timeElasped = block.timestamp + startAt;
+        uint discount = discountRate * timeElasped;
+        price = startingPrice - discount;
     }
-
-    modifier onlyAdmin(bytes32 _role){
-        require(user[_role][msg.sender], "Unauthorized user");
-        _;
+    function buy() external payable{
+        require(block.timestamp < expiresAt, "auction has expired");
+        uint price = getPrice();
+        require(msg.value >= price, "Insufficient funds");
+        nft.transferFrom(seller, msg.sender, nftId);
+        uint refund = msg.value - price;
+        if (refund > 0){
+            payable(msg.sender).transfer(refund);
+             selfdestruct(seller); }
     }
-
-    function _grantRole(bytes32 _role, address _user) internal{
-        user[_role][_user] = true;
-    }
-
-    function addUser(bytes32 _role, address _user, string calldata _reason) external onlyAdmin(SUPERUSER) {
-        user[_role][_user] = true;
-        emit UserAdded(_role, _user, _reason);
-
-    }
-
-    function removeUser(bytes32 _role, address _user, string calldata _reason) external onlyAdmin(SUPERUSER){
-        user[_role][_user] = false;
-        emit UserRemoved(_role, _user, _reason);
-    
-    }
+   
 }
